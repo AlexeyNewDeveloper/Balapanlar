b1436a02c3d5b27c636b45b66c4e492f4f4c801c
Alexey Yanchev <yanchevalexey@yandex.ru>
2022-08-29 19:38:22 +0300

    Сделал скролл без js

--- src/components/courses.js
+++ src/components/courses.js
@@ -3,7 +3,5 @@ import { courseOpenBtn, coursePopup, popupCloseButton } from './constants.js';
 
 courseOpenBtn.addEventListener("click", function () {
   openPopup(coursePopup);
-  if(window.matchMedia('(max-width: 400px)').matches) {
-    popupCloseButton.classList.add('popup__close-btn_position_fixed');
-  }
+
 });
