e0834b82ef8d95fb3f0e69fb80e764c8efb0b750
Alexey Yanchev <yanchevalexey@yandex.ru>
2022-08-29 19:30:52 +0300

    Скролл попапа

--- src/components/constants.js
+++ src/components/constants.js
@@ -5,5 +5,6 @@ export const popupCloseBtn = document.querySelectorAll(".popup__close-btn");
 
 export const coursePopup = document.querySelector(".popup-course");
 export const courseOpenBtn  = document.querySelector(".course-item__browse");
+export const popupCloseButton = coursePopup.querySelector('.popup__close-btn');
 
 export const partnerPopup = document.querySelector(".popup-partner");
--- src/components/courses.js
+++ src/components/courses.js
@@ -1,6 +1,9 @@
 import { openPopup } from './utils.js';
-import { courseOpenBtn, coursePopup } from './constants.js';
+import { courseOpenBtn, coursePopup, popupCloseButton } from './constants.js';
 
 courseOpenBtn.addEventListener("click", function () {
   openPopup(coursePopup);
+  if(window.matchMedia('(max-width: 400px)').matches) {
+    popupCloseButton.classList.add('popup__close-btn_position_fixed');
+  }
 });
